f3041650bf55069b6c558490ddf6e92a38bd0d4c
Dmitry Grechka <dmitry@grechka.family>
2022-09-02 00:03:09 +0300

    fixing yarn.lock

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,12 @@ var cardStorageURL: string
 var solrGatewayURL: string
 if (development) {
   console.log("Running in development mode")
-  cardStorageURL = "http://10.0.4.12:31642"
-  solrGatewayURL = "http://10.0.4.12:30069"
+  // cardStorageURL = "http://10.0.4.12:31642"
+  // solrGatewayURL = "http://10.0.4.12:30069"
   // cardStorageURL = "http://cassandra-rest-api.kashtanka"
   // solrGatewayURL = "http://search-gateway.kashtanka"
-  //cardStorageURL = "https://kashtanka.pet/api/storage"
-  //solrGatewayURL = "https://kashtanka.pet/api/search"
+  cardStorageURL = "https://kashtanka.pet/api/storage"
+  solrGatewayURL = "https://kashtanka.pet/api/search"
 } else {
   cardStorageURL = "api/storage"
   solrGatewayURL = "api/search"
